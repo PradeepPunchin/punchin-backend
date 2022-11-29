7876627bbc98835a20f25acd0600d44e73a3de57
Tarun Jangra <tarun.jangra@oodles.io>
2022-11-29 22:39:42 +0530

    eruka or api gateway removed.

--- seed/APIGateway/src/main/java/com/oodles/apigateway/repository/RedisUserRoleRepo.java
+++
@@ -1,9 +0,0 @@
-package com.oodles.apigateway.repository;
-
-import com.oodles.apigateway.domain.RedisUserRole;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RedisUserRoleRepo extends CrudRepository<RedisUserRole, String> {
-    RedisUserRole findByToken(String token);
-
-}

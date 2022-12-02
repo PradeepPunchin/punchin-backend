bd893c9ca56699c140d07224818c515959573b35
Tarun Jangra <tarun.jangra@oodles.io>
2022-12-02 16:09:48 +0530

    new cors.

--- seed/BackendAPIs/src/main/java/com/punchin/config/CorsConfig.java
+++ seed/BackendAPIs/src/main/java/com/punchin/config/CorsConfig.java
@@ -20,8 +20,8 @@ import java.io.IOException;
  *
  */
 
-@Configuration
-@Order(Ordered.HIGHEST_PRECEDENCE)
+//@Configuration
+//@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsConfig extends OncePerRequestFilter {
     private static final Logger log = LoggerFactory.getLogger(CorsConfig.class);
     private static final String ALLOWED_METHODS = "GET, POST, PUT, DELETE, OPTIONS";
--- seed/BackendAPIs/src/main/java/com/punchin/security/SecurityConfiguration.java
+++ seed/BackendAPIs/src/main/java/com/punchin/security/SecurityConfiguration.java
@@ -17,6 +17,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -47,6 +52,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .ignoring()
                 .antMatchers("/h2-console/**/**");
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 

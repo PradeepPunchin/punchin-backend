d2558c19232bc39cef3ea8e6f1efe00416a2c27a
sukesh <sukesh.chakraborty@oodles.io>
2022-12-21 10:52:23 +0530

    upload document from agent side

--- seed/BackendAPIs/src/main/java/com/punchin/utility/constant/MessageCode.java
+++ seed/BackendAPIs/src/main/java/com/punchin/utility/constant/MessageCode.java
@@ -39,6 +39,6 @@ public class MessageCode {
     public static final String SEARCHED_CLAIM_DATA_FETCHED_SUCCESS = "Searched claim data fetch succeeded";
     public static final String NO_RECORD_FOUND = "No record found";
     public static final String ERROR_SEARCHED_CLAIM_DATA_FETCHED = "Error while Searching claim data";
-
+    public static final String ERROR_UPLOAD_DOCUMENT = "Error while uploading document";
 
 }

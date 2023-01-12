f2bcc4e38f7362a6957ab3a201438a350fc8df5b
Tarun Jangra <tarun.jangra@oodles.io>
2023-01-12 13:52:07 +0530

    query issue resolve

--- seed/BackendAPIs/src/main/java/com/punchin/repository/UserRepository.java
+++ seed/BackendAPIs/src/main/java/com/punchin/repository/UserRepository.java
@@ -41,5 +41,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Long findTopByStateIgnoreCaseOrderById(String state);
 
     @Query(nativeQuery = true, value = "SELECT EXISTS(SELECT id FROM users WHERE role = 'VERIFIER' and LOWER(state)=LOWER((SELECT state FROM pin_code_state WHERE pin_code=:pinCode)) LIMIT 1)")
-    boolean existsByPinCode(String borrowerPinCode);
+    boolean existsByPinCode(String pinCode);
 }

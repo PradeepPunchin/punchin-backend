dd963f1330309544fee6e13c32be8d0b7afb3938
sukesh <sukesh.chakraborty@oodles.io>
2023-01-13 11:51:37 +0530

    ccode changes in agent document status

--- seed/BackendAPIs/src/main/java/com/punchin/dto/AgentDocumentStatusDTO.java
+++ seed/BackendAPIs/src/main/java/com/punchin/dto/AgentDocumentStatusDTO.java
@@ -4,7 +4,14 @@ import lombok.Data;
 
 @Data
 public class AgentDocumentStatusDTO {
-    private String agentDocName;
+    private String signedClaimDocument = "NOT_UPLOADED";
+    private String deathCertificate = "NOT_UPLOADED";
+    private String bankAccountProof = "NOT_UPLOADED";
+    private String additionalDoc = "NOT_UPLOADED";
+    private String relationshipDoc = "NOT_UPLOADED";
+    private String guardianIdProof = "NOT_UPLOADED";
+    private String guardianAddressProof = "NOT_UPLOADED";
+    private String nomineeKycProof = "NOT_UPLOADED";
+    private String borrowerKycProof = "NOT_UPLOADED";
 
-    private String status = "NOT UPLOADED";
 }

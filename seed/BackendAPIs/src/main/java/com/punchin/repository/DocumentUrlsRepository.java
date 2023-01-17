82819d3bfe1d1bd55a0a61cfc66a96bd69834a17
Tarun Jangra <tarun.jangra@oodles.io>
2023-01-17 17:21:23 +0530

    issues resovled multiple same imags

--- seed/BackendAPIs/src/main/java/com/punchin/repository/DocumentUrlsRepository.java
+++ seed/BackendAPIs/src/main/java/com/punchin/repository/DocumentUrlsRepository.java
@@ -18,6 +18,6 @@ public interface DocumentUrlsRepository extends JpaRepository<DocumentUrls, Long
     @Query(nativeQuery = true, value = "SELECT * FROM document_urls du INNER JOIN claim_documents_document_urls cddu ON du.id = cddu.document_urls_id INNER JOIN claim_documents cd ON cddu.claim_documents_id = cd.id WHERE cd.claims_data_id=:id AND cd.upload_side_by=:agent AND cd.is_active=:b AND cd.agent_doc_type=:docType")
     List<DocumentUrls> findDocumentUrlsByClaimDocument(Long id, String agent, boolean b, String docType);
 
-    @Query(nativeQuery = true, value = "select * from document_urls du inner join claim_documents_document_urls cddu ON du.id =cddu.document_urls_id where cddu.claim_documents_id in (SELECT id FROM claim_documents WHERE claims_data_id=37 AND upload_side_by='agent' AND is_active=true AND agent_doc_type=:docType)")
+    @Query(nativeQuery = true, value = "select * from document_urls du inner join claim_documents_document_urls cddu ON du.id =cddu.document_urls_id where cddu.claim_documents_id in (SELECT id FROM claim_documents WHERE claims_data_id=:id AND upload_side_by=:agent AND is_active=:b AND agent_doc_type=:docType)")
     List<DocumentUrls> findAllDocumentAccordingToClaimDocuments(Long id, String agent, boolean b, String docType);
 }
